a0cd39db9d006e6a8501f6fc4f5de02165977b49
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-11-12 23:33:38 +0330

    first problem complated (splitStrin....)

---
+++ splitStringsBySeparator-easy.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const splitWordsBySeparator = (words, separator) => {
+  return words.flatMap(word => word.split(separator)).filter(item => item !== '');
+};
+
+console.log(splitWordsBySeparator(["$easy$", "$problem$"], "$"));
