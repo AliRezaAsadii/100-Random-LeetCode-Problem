30689b23780cacdb9fd93e8dbc4b0079892680a9
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-11-12 23:34:19 +0330

    edit file

--- splitStringsBySeparator-easy.js
+++ splitStringsBySeparator-easy.js
@@ -3,5 +3,3 @@
 const splitWordsBySeparator = (words, separator) => {
   return words.flatMap(word => word.split(separator)).filter(item => item !== '');
 };
-
-console.log(splitWordsBySeparator(["$easy$", "$problem$"], "$"));
